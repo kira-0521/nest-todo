d84ffaa1ad7a1eb269a3ae1acdb373e9059ece01
kira-0521 <kiratanaka0521@gmail.com>
2023-01-02 12:14:23 +0900

    ✨ [ feature ] config, typeorm module settings

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from 'src/config/configuration';
+import { TypeOrmConfigService } from 'src/services/typeorm/type-orm-config.service';
+
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [
+    TodoModule,
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
-// import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TodoController } from 'src/controllers/todo/todo.controller';
-// import { Todo } from 'src/entities/todo.entity';
-// import { TodoRepository } from 'src/repositories/todo.repository';
+import { Todo } from 'src/entities/todo.entity';
+import { TodoRepository } from 'src/repositories/todo.repository';
 import { TodoService } from 'src/services/todo/todo.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Todo])],
   controllers: [TodoController],
-  // providers: [TodoService, TodoRepository],
-  providers: [TodoService],
+  providers: [TodoService, TodoRepository],
 })
 export class TodoModule {}
