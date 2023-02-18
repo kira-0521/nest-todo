95396ddd9c44fcb56b12d7495e7aeecc5811851c
kira-0521 <kiratanaka0521@gmail.com>
2023-02-18 16:42:16 +0900

    🏷️ add: レスポンス用TODO Type

--- src/@types/todo.d.ts
+++ src/@types/todo.d.ts
@@ -2,6 +2,9 @@ export interface Todo {
   id: string;
   title: string;
   isCompleted: boolean;
+  imgUrl: string;
   createdAt: string;
   updatedAt: string;
 }
+
+export type ReturnTodoType = Omit<Todo, 'createdAt' | 'updatedAt'>;
