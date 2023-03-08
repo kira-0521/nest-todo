3ed2b71cc74658a50ac93fe282b4257c9cf98bdb
kira-0521 <kiratanaka0521@gmail.com>
2023-03-08 20:19:29 +0900

    feat: constant metadata

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,6 @@
 export const TODO_IMAGE_FILE_PATH = './files';
 export const BASE_URL = 'http://localhost:3090';
+
+export const META = {
+  roles: 'roles',
+};
--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -1,3 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
+import { META } from 'src/constants';
 
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
+export const Roles = (...roles: string[]) => SetMetadata(META.roles, roles);
